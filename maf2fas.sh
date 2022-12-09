4a269604e0ce54d7f381acca00c2b119cd3dfed2
Subir <subirshakya@users.noreply.github.com>
2022-12-09 15:31:25 -0500

    Update maf2fas.sh
    Added ability to parallelize script

--- maf2fas.sh
+++ maf2fas.sh
@@ -1,44 +1,74 @@
-while getopts i:o:n:b: flag; do
-	case "${flag}" in
-		i) input=${OPTARG};;
-		o) output=${OPTARG};;
-		n) name=${OPTARG};;
-		b) bed=${OPTARG};;
-	esac
+usage()
+{
+    echo "usage: <command> options:<i|o|n|b|s|j>"
+        echo
+        echo "i: input folder"
+        echo "o: output folder"
+        echo "n: filename to append (default = file)"
+        echo "b: bed file"
+        echo "s: reference species"
+        echo "j: number of parallel nodes (defaults = 1)"
+}
+
+faster () {
+        input=$1
+        line=$2
+        scaffold=$3
+        IFS="     " read -r start stop <<< "$4"
+        stop2=$(($stop+1))
+        sed -n "$start,${stop}p;${stop2}q" $line > $input/temp_$scaffold/$scaffold-$start-$stop.fa
+        pos_start=$(awk 'NR==1{print $3}' $input/temp_$scaffold/$scaffold-$start-$stop.fa)
+        pos_intv=$(awk 'NR==1{print $4}' $input/temp_$scaffold/$scaffold-$start-$stop.fa)
+        pos_stop=$(($pos_start+$pos_intv-1))
+        sed -i "s/^s\t\([A-za-z_0-9]*\).[A-Za-z0-9_,.\/:\-]*\t[0-9]*\t[0-9]*\t[+-]\t[0-9]*\t\([ATCG]*\)/>\1\n\2/" $input/temp_$scaffold/$scaffold-$start-$stop.fa
+        mv $input/temp_$scaffold/$scaffold-$start-$stop.fa $input/temp_$scaffold/$scaffold-$pos_start-$pos_stop.fa
+        }
+export -f faster
+
+name="file"
+parallel=1
+no_args="true"
+while getopts hi:o:n:b:s:j: flag; do
+        case "${flag}" in
+                i) input=${OPTARG};;
+                o) output=${OPTARG};;
+                n) name=${OPTARG};;
+                b) bed=${OPTARG};;
+                s) refsp=${OPTARG};;
+                j) parallel=${OPTARG};;
+                h) usage
+                exit 0;;
+                (*)
+                        usage
+                        exit 0;;
+        esac
+        no_args="false"
 done
 
-if [ ! -d $input/temp${SLURM_ARRAY_TASK_ID} ]; then
-	mkdir -p $input/temp${SLURM_ARRAY_TASK_ID};
-fi
+[[ "$no_args" == "true" ]] && { usage; exit 1; }
 
 if [ ! -d $output ]; then
-	mkdir -p $output;
+        mkdir -p $output;
 fi
 
 if [ ! -f $input/all_maf_files.txt ]; then
-	ls $input/*.maf > $input/all_maf_files.txt
+        ls $input/*.maf > $input/all_maf_files.txt
 fi
 
 while IFS= read -r line; do
         scaffold=$(echo $line | sed "s/^.*${name}_\([A-Za-z0-9_\.]*\).*/\1/" | sed "s/\.maf//")
-        echo $scaffold
-        grep -n "^a" $line | cut -f1 -d: > $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt
-        echo "$(paste $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt <(sed 1,1d $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt))" > $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt
-        echo "$(awk '{print ($1+1 "     " $2-2)}' $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt)" > $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt
+                echo $scaffold
+                if [ ! -d $input/temp_$scaffold ]; then
+                                mkdir -p $input/temp_$scaffold;
+                fi
+        grep -n "^a" $line | cut -f1 -d: > $input/temp_$scaffold/$scaffold-alns.txt
+        echo "$(paste $input/temp_$scaffold/$scaffold-alns.txt <(sed 1,1d $input/temp_$scaffold/$scaffold-alns.txt))" > $input/temp_$scaffold/$scaffold-alns.txt
+        echo "$(awk '{print ($1+1 "     " $2-2)}' $input/temp_$scaffold/$scaffold-alns.txt)" > $input/temp_$scaffold/$scaffold-alns.txt
         fin_val=$(wc -l < $line)
         fin_val=$((fin_val-1))
-        echo "$(sed "s/\-[2]/$fin_val/" $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt)" > $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt
-        while IFS="     " read -r start stop; do
-                stop2=$(($stop+1))
-                sed -n "$start,${stop}p;${stop2}q" $line > $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-$start-$stop.fa
-                pos_start=$(awk 'NR==1{print $3}' $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-$start-$stop.fa)
-                pos_intv=$(awk 'NR==1{print $4}' $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-$start-$stop.fa)
-                pos_stop=$(($pos_start+$pos_intv-1))
-                sed -i "s/^s\t\([A-za-z_]*\).[A-Za-z0-9_,.\/-]*\t[0-9]*\t[0-9]*\t[+-]\t[0-9]*\t\([ATCG]*\)/>\1\n\2/" temp_${SLURM_ARRAY_TASK_ID}/$scaffold-$start-$stop.fa
-                mv $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-$start-$stop.fa $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-$pos_start-$pos_stop.fa
-        done < "$input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-alns.txt"
-        grep -P "${scaffold}\t" $bed > $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-cnee.bed
-        python ./organize_fasta.py --bed $input/temp_${SLURM_ARRAY_TASK_ID}/$scaffold-cnee.bed --fasta_header $scaffold --out_folder $output --temp_folder $input/temp_${SLURM_ARRAY_TASK_ID}
-        #rm $input/temp_${SLURM_ARRAY_TASK_ID}/*.*
-        find $input/temp_${SLURM_ARRAY_TASK_ID}/ -type f -delete
-done < "$input/all_maf_files.txt"        
+        echo "$(sed "s/\-[2]/$fin_val/" $input/temp_$scaffold/$scaffold-alns.txt)" > $input/temp_$scaffold/$scaffold-alns.txt
+        parallel -j $parallel faster $input $line $scaffold :::: "$input/temp_$scaffold/${scaffold}-alns.txt"
+        grep -P "${scaffold}\t" $bed > $input/temp_$scaffold/$scaffold-cnee.bed
+        python ./organize_fasta.py --bed $input/temp_$scaffold/$scaffold-cnee.bed --fasta_header $scaffold --out_folder $output --temp_folder $input/temp_$scaffold/ --ref_species $refsp
+        rm -r $input/temp_$scaffold
+done < "$input/all_maf_files.txt"
