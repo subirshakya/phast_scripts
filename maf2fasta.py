7bf714f0107d171d6010f900b55437ec1676c873
gwct <greggwct@gmail.com>
2023-01-20 10:22:28 -0500

    added the edited MafIO script to the system path in the maf2fasta script and explicitly call it from the script

--- maf2fasta.py
+++ maf2fasta.py
@@ -1,7 +1,11 @@
 import argparse
 import pandas as pd
 from Bio import AlignIO
+import sys
 import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), 'lib'))
+import MafIO_edited
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--maf",
@@ -16,13 +20,17 @@ parser.add_argument("--chromosome",
                     help="chromosome/scaffold name")
 args = parser.parse_args()
 
-idx = AlignIO.MafIO.MafIndex(args.maf + "index", args.maf, args.ref_species)
+print("# generating maf index...")
+idx = MafIO_edited.MafIndexEdit(args.maf + "index", args.maf, args.ref_species)
 
+print("# reading bed file...")
 bed_df = pd.read_csv(args.bed, sep='\t', header=None)
 
+print("# creating output folder: " + args.out_folder)
 if not os.path.exists(args.out_folder):
    os.makedirs(args.out_folder)
 
+print("# beginning fasta conversion...")
 for index, row in bed_df.iterrows():
     start, stop, ce_name = row[1], row[2], row[3]
     #print (ce_name)
